41af6d30544ae011692458c04d4482a5f9aa13ff
nilesh <nilesh@365aitech.com>
2025-08-20 12:08:56 +0530

    new

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.jackson.app',
   appName: 'Jackson',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    allowNavigation: [
+      'http://94.249.151.176:4001'
+    ],
+    cleartext: true
+  }
 };
 
 export default config;
